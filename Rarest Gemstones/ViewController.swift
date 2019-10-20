8acf9157f7152f969a79799c0f366b84682cd91f
Mirko Cukich <mirkocukich@gmail.com>
2019-10-20 13:00:45 -0500

    Asset images used added

--- Rarest Gemstones/ViewController.swift
+++ Rarest Gemstones/ViewController.swift
@@ -5,6 +5,8 @@
 //  Created by Mirko Cukich on 10/19/19.
 //  Copyright © 2019 Digital Mirko. All rights reserved.
 //
+//  Swift Rarest Gemstones Collection Views Image Gallery Demo - Demo 25 of 30
+
 
 import UIKit
 
