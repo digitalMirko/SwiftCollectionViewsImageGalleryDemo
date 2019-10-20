6ef32de400105a82ec38fda65aa686040301a0ad
Mirko Cukich <mirkocukich@gmail.com>
2019-10-20 13:11:27 -0500

    Collection View Controller, Navigation Controller, Collection View Controller and View Cell added and associated

---
+++ Rarest Gemstones/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  Rarest Gemstones
+//
+//  Created by Mirko Cukich on 10/20/19.
+//  Copyright © 2019 Digital Mirko. All rights reserved.
+//
+//  Swift Rarest Gemstones Collection Views Image Gallery Demo - Demo 25 of 30
+
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
